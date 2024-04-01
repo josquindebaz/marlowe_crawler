d2b6e7d0afb43b2638db117b809346774a371952
Josquin Debaz <josquindebaz@protonmail.com>
2024-04-01 16:44:43 +0200

    Move france24 to controller object

--- france24.py
+++ france24.py
@@ -1,19 +1,12 @@
-import database
-
-from crawlers import crawl_link
+from Controller import Controller
 from parsers.France24Parser import France24Parser
-from rss_parser import get_rss_soup
 
 if __name__ == "__main__":
-    rss_link = "https://www.france24.com/fr/rss"
-    rss_content = get_rss_soup(crawl_link(rss_link))
-
-    parser = France24Parser()
-    parser.process_articles(rss_content, "france24")
-
-    print("France24", rss_content["description"], " -> crawled ", len(parser.articles),
-          " on ", len(rss_content["articles"]))
-
-    for article in parser.articles:
-        # print(f"Inserting {article.title}")
-        database.insert_in_table(article)
+    controller = Controller(
+        rss_links=[
+            'https://www.france24.com/fr/rss'
+        ],
+        parser=France24Parser(),
+        author="france24"
+    )
+    controller.run()
