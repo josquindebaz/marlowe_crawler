cdf353e4dbe5480702e0f1479c1adb96464f4fed
Josquin Debaz <josquindebaz@protonmail.com>
2024-04-01 11:28:01 +0200

    Clean france24

--- france24.py
+++ france24.py
@@ -1,34 +1,8 @@
-from bs4 import BeautifulSoup
-from datetime import datetime
-
 import database
-from models.Article import Article
+
 from crawlers import crawl_link
 from parsers.France24Parser import France24Parser
 from rss_parser import get_rss_soup
-
-
-def info_crawler(info):
-    """ From title description pubDate link enclosure
-        To Article
-    """
-    link = info.find("link").getText()
-    info_soup = BeautifulSoup(crawl_link(link), 'lxml')
-
-    paragraphs = [
-        paragraph.getText() for paragraph in info_soup.findAll("p")
-        if paragraph.attrs == {}
-    ]
-
-    return Article(
-        date=datetime.strptime(info.find("pubDate").getText(), "%a, %d %b %Y %H:%M:%S %Z"),
-        link=link,
-        title=info.find("title").getText(),
-        description=info.find("description").getText(),
-        content="\n".join(paragraphs),
-        author="france24"
-    )
-
 
 if __name__ == "__main__":
     rss_link = "https://www.france24.com/fr/rss"
